85a213f0d1c1a1ec1f9a08e791611eb7b74db5be
OTS <OTS>
2024-07-14 23:56:23 +0000

    update validation of request and ads

--- src/controllers/advertisments.controller/advertisments.validator.js
+++ src/controllers/advertisments.controller/advertisments.validator.js
@@ -45,11 +45,23 @@ export default {
             body('area').optional().not().isEmpty().withMessage(() => { return i18n.__('areaRequired') }),
             body('price').optional().optional().not().isEmpty().withMessage(() => { return i18n.__('priceRequired') }),
             
-            body('city').optional().not().isEmpty().withMessage(() => { return i18n.__('cityRequired') }),
-            body('district').optional().not().isEmpty().withMessage(() => { return i18n.__('districtRequired') }),
+            body('city').optional().not().isEmpty().withMessage(() => { return i18n.__('cityRequired') })
+            .custom(async(val)=>{
+                await checkExist(val, City, { deleted: false });
+                return true;
+            }),
+            body('district').optional().not().isEmpty().withMessage(() => { return i18n.__('districtRequired') })
+            .custom(async(val)=>{
+                await checkExist(val, Region, { deleted: false });
+                return true;
+            }),
             body('description').optional().not().isEmpty().withMessage(() => { return i18n.__('descriptionRequired') }),
 
-            body('user').optional().not().isEmpty().withMessage(() => { return i18n.__('userRequired') }),
+            body('user').optional().not().isEmpty().withMessage(() => { return i18n.__('userRequired') })
+            .custom(async(val)=>{
+                await checkExist(val, User, { status:'ACTIVE' });
+                return true;
+            }),
 
             body('itemType').optional().not().isEmpty().withMessage(() => { return i18n.__('itemTypeRequired') })
             .isIn(["ADD" , "REQUEST"]).withMessage(() => { return i18n.__('invalidType') })
